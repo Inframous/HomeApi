01de8cb0ac6ebb8e2a5813de988ec9c0a8428ddd
Gabriel P <mail.gabriel.pollak@gmail.com.com>
2023-12-28 08:17:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ sudo docker build -t "homeapi" .
 
 # Launch the container
 sudo docker run -d -p 8080:5000 --restart always --name homeapi homeapi
+
+echo 'Point your browser to "http://<docker_host_ip>:<port>/site/tami4setup" to get your API Key and configure the connection.'
+echo 'After that, sending a GET request to "http://<docker_host_ip>:<port>/api/tami4/boil" will send the boil command to your Tami4 device."
