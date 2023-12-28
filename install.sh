9f257150f478fac0e9368ba9b6ef83b1f51d5450
Gabriel P <mail.gabriel.pollak@gmail.com.com>
2023-12-28 09:58:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PORT=6500 
-
 # Clone the repository
 git clone https://github.com/Inframous/HomeApi.git
 cd HomeApi || exit
@@ -13,7 +11,7 @@ sudo docker build -t "homeapi" .
 sudo docker run -d -p 6500:5000 --restart always --name homeapi homeapi
 
 
-echo "Point your browser to 'http://<docker_host_ip>:6500/site/tami4setup' to get your API Key and configure the connection."
-echo "After that, sending a GET request to 'http://<docker_host_ip>:6500/api/tami4/boil' will send the boil command to your Tami4 device."
+echo 'Point your browser to "http://<docker_host_ip>:6500/site/tami4setup" to get your API Key and configure the connection.'
+echo 'After that, sending a GET request to "http://<docker_host_ip>:6500/api/tami4/boil" will send the boil command to your Tami4 device.'
 echo 'Enjoy your HOT beverages!'
 
